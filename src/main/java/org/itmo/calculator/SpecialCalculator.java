023bb38f46ab74e1bac9288b4bc0c3c8559123c4
Roggired <leshaego14@gmail.com>
2022-10-02 02:44:49 +0300

    Initial commit

---
+++ src/main/java/org/itmo/calculator/SpecialCalculator.java
@@ -0,0 +1,20 @@
+package org.itmo.calculator;
+
+public final class SpecialCalculator {
+    public SpecialCalculator(int memoryCellsSize) {
+        // напишите здесь код
+    }
+
+    public String calculate(String expression) {
+        // напишите здесь код
+        return ""; // не забудьте заменить "" на результат операции
+    }
+
+    public void putLastResultInMemory(int memoryCellIndex) {
+        // напишите здесь код
+    }
+
+    public void resetMemory(int memoryCellIndex) {
+        // напишите здесь код
+    }
+}
